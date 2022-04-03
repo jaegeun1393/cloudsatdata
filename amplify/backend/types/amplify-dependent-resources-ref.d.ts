f2d59bad7bd0c0df57a33895c1c7eadba23dc89d
jaegeun1393 <46430624+jaegeun1393@users.noreply.github.com>
2022-04-02 23:52:08 -0700

    update2

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -5,10 +5,21 @@ export type AmplifyDependentResourcesAttributes = {
             "Arn": "string",
             "Region": "string",
             "LambdaExecutionRole": "string"
+        },
+        "usermanagement": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
         }
     },
     "api": {
         "cloudsatapi": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        },
+        "cloudapi": {
             "RootUrl": "string",
             "ApiName": "string",
             "ApiId": "string"
