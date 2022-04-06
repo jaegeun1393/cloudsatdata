31f11de82aea5120d0b6d98bd5aa4c97fb09172b
jaegeun1393 <46430624+jaegeun1393@users.noreply.github.com>
2022-04-05 17:36:06 -0700

    Update Download.js

--- src/Download.js
+++ src/Download.js
@@ -49,33 +49,54 @@ function Downloads() {
 
 </section>
 
-<section className="py-20">
-  <div className="container items-center max-w-6xl px-4 px-10 mx-auto sm:px-20 md:px-32 lg:px-16">
-    <div className="flex flex-wrap items-center -mx-3">
-      <div className="order-1 w-full px-3 lg:w-1/2 lg:order-0">
-        <div className="w-full lg:max-w-md">
-          <h2 className="mb-4 text-3xl font-bold leading-tight tracking-tight sm:text-4xl font-heading">Helps to reduce the waiting time for students.</h2>
-          <p className="mb-4 font-medium tracking-tight text-gray-400 xl:mb-6">You can easily build the student's study pathway!</p>
-          <ul>
-            <li className="flex items-center py-2 space-x-4 xl:py-3">
-              <svg className="w-8 h-8 text-pink-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z"></path></svg>
-              <span className="font-medium text-gray-500">Faster Processing and Delivery</span>
-            </li>
-            <li className="flex items-center py-2 space-x-4 xl:py-3">
-              <svg className="w-8 h-8 text-yellow-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"></path></svg>
-              <span className="font-medium text-gray-500">Out of the Box Tracking and Monitoring</span>
-            </li>
-            <li className="flex items-center py-2 space-x-4 xl:py-3">
-              <svg className="w-8 h-8 text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"></path></svg>
-              <span className="font-medium text-gray-500">100% Protection and Security for Your App</span>
-            </li>
-          </ul>
-        </div>
+<div class="full-row flex full-col gap-5 items-center justify-center bg-white py-40">
+
+  <card class="border-gray-300 border-2 rounded-xl w-[30rem] py-7 px-5">
+    <div class="grid grid-cols-6 gap-3">
+      
+      <div class="col-span-2">
+        <img src="https://events.duolingo.com/images/why_global.svg" />
       </div>
-      <div className="w-full px-3 mb-12 lg:w-1/2 order-0 lg:order-1 lg:mb-0"><img className="mx-auto sm:max-w-sm lg:max-w-full" src="https://cdn.devdojo.com/images/november2020/feature-graphic.png" alt="feature image" /></div>
+
+      <div class="col-span-4">
+        <p class="text-gray-700 font-bold"> Did you know our platform is 100% FREE? </p>
+        <p class="text-gray-500 mt-4"> Yes it is! We are offering Unlimited spaces, and uploading. </p>
+      </div>
+
     </div>
-  </div>
-</section>
+  </card>
+
+  <card class="border-gray-300 border-2 rounded-xl w-[30rem] py-7 px-5">
+    <div class="grid grid-cols-6 gap-3">
+      
+      <div class="col-span-4">
+        <p class="text-gray-700 font-bold"> Create an impact </p>
+        <p class="text-gray-500 mt-4"> Reduce the waiting time to get the response back from the teacher! </p>
+      </div>
+
+      <div class="col-span-2">
+        <img src="https://events.duolingo.com/images/why_impact.svg" />
+      </div>
+
+    </div>
+  </card>
+
+  <card class="border-gray-300 border-2 rounded-xl w-[30rem] py-7 px-5">
+    <div class="grid grid-cols-6 gap-3">
+      
+      <div class="col-span-2">
+        <img src="https://events.duolingo.com/images/why_access.svg" />
+      </div>
+
+      <div class="col-span-4">
+        <p class="text-gray-700 font-bold"> High Security Services </p>
+        <p class="text-gray-500 mt-4"> Enjoy to access our service wit the strong security service! </p>
+      </div>
+
+    </div>
+  </card>
+
+</div>
 
   </div>
   )
