52472c02bb4e27802b504526c223792a7a7da8d2
jaegeun1393 <46430624+jaegeun1393@users.noreply.github.com>
2022-04-06 16:45:20 -0700

    .

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import './css/index.css';
 
 import { DataStore } from '@aws-amplify/datastore';
 import { Usersat } from './models';
-Amplify.configure(awsconfig);
+Amplify.configure(config);
 Auth.configure(config);
 
 API.configure({
