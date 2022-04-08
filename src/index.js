426845a34c080672b4f84b0c0eb84068f97edec7
jaegeun1393 <46430624+jaegeun1393@users.noreply.github.com>
2022-04-07 19:12:07 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom'; 
-import Amplify from '@aws-amplify/core'
-import { Auth } from '@aws-amplify/auth'
-import { API } from 'aws-amplify';
+import { Amplify, Auth, API } from 'aws-amplify'
 import awsconfig from './aws-exports';
 
 import App from './App';
