44ff8c31190996f989a4e515aff4de6528140251
jaegeun1393 <46430624+jaegeun1393@users.noreply.github.com>
2022-04-10 17:38:15 -0700

    image upload

--- src/uploadsat.js
+++ src/uploadsat.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Navigate } from "react-router-dom";
-import {Auth} from 'aws-amplify';
+import {Auth, API} from 'aws-amplify';
 
 
 import './css/App.css';
@@ -11,8 +11,25 @@ class Uploadsat extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      loginstate: ""
+      loginstate: "",
+      profileImg:'https://static.thenounproject.com/png/643663-200.png'
     };
+    this.imageHandler = this.imageHandler.bind(this)
+  }
+
+  imageHandler = (e) => {
+    const reader = new FileReader();
+    reader.onload = () =>{
+      if(reader.readyState === 2){
+        this.setState({profileImg: reader.result})
+      }
+    }
+    reader.readAsDataURL(e.target.files[0])
+  };
+
+  async uploadfile() {
+    const data = await API.get('satgrading', '/satgrading/')
+    console.log(data);
   }
 
   async componentDidMount() {
@@ -26,6 +43,7 @@ class Uploadsat extends Component {
 
   render() {
     if (this.state.loginstate === false) return <Navigate to="/Login" />
+    const { profileImg } = this.state
   return (
     <div>
       { this.state.loginstate === true && (
@@ -39,16 +57,16 @@ class Uploadsat extends Component {
 			</h2>
 			<p className="mt-2 text-sm text-gray-400">Upload the picture of the SAT answersheet.</p>
 		</div>
-        <form className="mt-8 space-y-3" action="#" method="POST">
+    <img src={profileImg} alt="" className="flex items-center m-auto"/>
+
+        <form className="mt-8 space-y-3" action="#">
                     <div className="grid grid-cols-1 space-y-2">
                         <div className="flex items-center justify-center w-full">
                             <label className="flex flex-col rounded-lg border-4 border-dashed w-full h-60 p-10 group text-center">
                                 <div className="h-full w-full text-center flex flex-col items-center justify-center items-center  ">
-                                    <div className="flex flex-auto max-h-48 w-2/5 mx-auto -mt-10">
-                                    </div>
-                                    <p className="pointer-none text-gray-500 "><span className="text-sm">Drag and drop</span> files here <br /> or <a href="" id="" className="text-blue-600 hover:underline">select a file</a> from your computer</p>
+                                    <p className="pointer-none text-gray-500 "><span className="text-sm">Drag and drop</span> files here <br /> or <a className="text-blue-600 hover:underline">select a file</a> from your computer</p>
                                 </div>
-                                <input type="file" className="hidden" />
+                                <input type="file" className="hidden"  onChange={this.imageHandler}/>
                             </label>
                         </div>
                     </div>
@@ -56,7 +74,7 @@ class Uploadsat extends Component {
                                 <span>File type: Jpg, and png</span>
                             </p>
                     <div>
-                        <button type="submit" className="my-5 w-full flex justify-center bg-blue-500 text-gray-100 p-4  rounded-full tracking-wide
+                        <button type="button" className="my-5 w-full flex justify-center bg-blue-500 text-gray-100 p-4  rounded-full tracking-wide
                                     font-semibold  focus:outline-none focus:shadow-outline hover:bg-blue-600 shadow-lg cursor-pointer transition ease-in duration-300">
                         Upload
                     </button>
